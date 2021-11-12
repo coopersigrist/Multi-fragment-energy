d1c104b4b9804550351243ca93af21470b00f0e4
Xingyu Bian <xingyubiancyberland@gmail.com>
2021-11-12 17:05:35 -0500

    Moved over some code to GAE.py Main program is still in Dataset.py

--- Experiments/Experiment Autoencoding/Dataset.py
+++ Experiments/Experiment Autoencoding/Dataset.py
@@ -1,10 +1,10 @@
 from torch_geometric.datasets import QM9
 import torch
-from torch_geometric.nn import GAE, GCNConv
 from torch.optim import Adam
 from torch_geometric.loader import DataLoader
 from torch_geometric.utils import train_test_split_edges
 import math
+from GAE import build_model
 
 # loading the QM9 dataset
 dataset = QM9(root="tmp/QM9")
@@ -25,27 +25,13 @@ test_dataset = dataset[train_idx:]
 # train_loader = DataLoader(train_dataset, batch_size=32)
 # test_loader = DataLoader(test_dataset, batch_size=32)
 
-'''implementations for the graph autoencoder
-   first we define the encoder we are using '''
-
-class GCNEncoder(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(GCNEncoder, self).__init__()
-        self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=True)
-        self.conv2 = GCNConv(2 * out_channels, out_channels, cached=True)
-
-    def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index).relu()
-        return self.conv2(x, edge_index)
-
-
 # defining some variables for training the model
 num_features = dataset.num_features
 encoder_out = 2
 epochs = 50
 
 # define the graph autoencoder
-model = GAE(GCNEncoder(num_features, encoder_out))
+model = build_model(num_features, encoder_out)
 
 # defining the optimizer
 optimizer = Adam(model.parameters(), lr=0.01)
@@ -61,7 +47,6 @@ def train():
         optimizer.step()
         optimizer.zero_grad()
         print(float(loss))
-
 
 # iterating over the epochs
 for epoch in range(1, epochs+1):
--- Experiments/Experiment Autoencoding/GAE.py
+++ Experiments/Experiment Autoencoding/GAE.py
@@ -1,1 +1,21 @@
 # Will hold the code for the model code, will move to here in the future
+from torch_geometric.datasets import QM9
+import torch
+from torch_geometric.nn import GAE, GCNConv
+
+# define the encoder
+class GCNEncoder(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GCNEncoder, self).__init__()
+        self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=True)
+        self.conv2 = GCNConv(2 * out_channels, out_channels, cached=True)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        return self.conv2(x, edge_index)
+
+
+# define the GAE model
+def build_model(encoder_in, encoder_out):
+    model = GAE(GCNEncoder(encoder_in, encoder_out))
+    return model
