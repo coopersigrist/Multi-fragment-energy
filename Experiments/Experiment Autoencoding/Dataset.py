57b6efce871fca231064f1771cab58efd3120beb
Xingyu Bian <xingyubiancyberland@gmail.com>
2021-11-12 15:22:17 -0500

    Added training there is still an index issue

--- Experiments/Experiment Autoencoding/Dataset.py
+++ Experiments/Experiment Autoencoding/Dataset.py
@@ -1,13 +1,13 @@
 from torch_geometric.datasets import QM9
 import torch
-import torch_geometric.transforms as T
-from torch_geometric.nn import GCNConv
-from torch_geometric.nn import GAE
+from torch_geometric.nn import GAE, GCNConv
+from torch.optim import Adam
 from torch_geometric.loader import DataLoader
+from torch_geometric.utils import train_test_split_edges
 import math
+
 # loading the QM9 dataset
 dataset = QM9(root="tmp/QM9")
-print(dataset)
 
 # 130,831 graphs are in the QM9 dataset
 num_of_graphs = len(dataset)
@@ -22,8 +22,8 @@ train_dataset = dataset[:train_idx]
 test_dataset = dataset[train_idx:]
 
 # training and testing loaders
-train_loader = DataLoader(train_dataset, batch_size=32)
-test_loader = DataLoader(test_dataset, batch_size=32)
+# train_loader = DataLoader(train_dataset, batch_size=32)
+# test_loader = DataLoader(test_dataset, batch_size=32)
 
 '''implementations for the graph autoencoder
    first we define the encoder we are using '''
@@ -46,3 +46,20 @@ epochs = 50
 
 # define the graph autoencoder
 model = GAE(GCNEncoder(num_features, encoder_out))
+
+# defining the optimizer
+optimizer = Adam(model.parameters(), lr=0.01)
+
+# training the model
+def train():
+    model.train()
+    for data in train_dataset:
+        z = model.encode(data.x, data.edge_index)
+        loss = model.recon_loss(z, data.edge_index)
+        # backprop
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        print(float(loss))
+
+train()
